5536031401a5001a77ca8ae1c575d595bef66ef5
Clay <claymcintosh@gmail.com>
2022-02-08 11:09:06 +1000

    Very basic README.md created.

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Homework-09-Node.js-Professional-README-Generator
+# Clays project
+
+    undefined
+    
+    ## Description
+    
+    undefined
+    
+    ## Table of Contents
+    
+    1. [Installation](#installation)
+    2. [Usage](#usage)
+    3. [License](#license)
+    4. [Contributing](#contributing)
+    5. [Tests](#tests)
+    6. [Questions](#questions)
+    
+    
+    ## Installation
+    
+    undefined
+    
+    ## Usage
+    
+    undefined
+    
+    ## License
+    
+    undefined
+    
+    ## Contributing
+    
+    undefined
+    
+    ## Tests
+    
+    undefined
+    
+    ## Questions
+    
+    undefined
+    
+    If you have any questions, you can reach me on my [GitHub account](https://undefined.io) or at my [email](mailto:undefined).
+    
--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-console.log("Hello World");
+const inquirer = require("inquirer");
+const fs = require("fs");
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is your project title?",
+    },
+  ])
+  .then(function (answer) {
+    const readme = `# ${answer.projectTitle}
+
+    ${answer.badge}
+    
+    ## Description
+    
+    ${answer.description}
+    
+    ## Table of Contents
+    
+    1. [Installation](#installation)
+    2. [Usage](#usage)
+    3. [License](#license)
+    4. [Contributing](#contributing)
+    5. [Tests](#tests)
+    6. [Questions](#questions)
+    
+    
+    ## Installation
+    
+    ${answer.installation}
+    
+    ## Usage
+    
+    ${answer.usage}
+    
+    ## License
+    
+    ${answer.license}
+    
+    ## Contributing
+    
+    ${answer.contributing}
+    
+    ## Tests
+    
+    ${answer.tests}
+    
+    ## Questions
+    
+    ${answer.Questions}
+    
+    If you have any questions, you can reach me on my [GitHub account](https://${answer.gitHubName}.io) or at my [email](mailto:${answer.email}).
+    `;
+    fs.writeFile("README.md", readme, function (err) {
+      if (err) {
+        return console.log("Sorry, an ERROR occurred. Please try again.");
+      }
+      console.log("README.md created sucessfully!");
+    });
+  });
